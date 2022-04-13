9edebf39c46889874b58fa8890b78453e01158ce
Giovanni <giovannisouza216@gmail.com>
2022-04-13 10:51:09 -0300

    fe

---
+++ src/navigations/login.navigation.tsx
@@ -0,0 +1,16 @@
+  
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginStackParamList } from "../types/Screen.types";
+import { LoginScreen, CadastrarScreen } from "../Screens";
+
+const Stack = createStackNavigator<LoginStackParamList>();
+
+export default function LoginNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Cadastrar" component={CadastrarScreen} />
+    </Stack.Navigator>
+  );
+}
