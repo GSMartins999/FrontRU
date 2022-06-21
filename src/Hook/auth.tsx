5244ab81a07f2c50d6ee2f8e788602c58e7db447
Giovanni <giovannisouza216@gmail.com>
2022-06-21 08:24:52 -0300

    fe

--- src/Hook/auth.tsx
+++ src/Hook/auth.tsx
@@ -4,94 +4,93 @@ import React, {
    useCallback,
    useState,
    useEffect,
-
 } from "react";
-import { apiUser } from "../services/data_antigo";
-import api from "../services/data/User";
+import { apiUser } from "../services/data";
+import api from "../services/data/api";
 import { IAuthState, IAuthContextData } from "../interfaces/User.interface";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AuthContext = createContext<IAuthContextData>({} as IAuthContextData);
 
-const AuthProvider: React.FC = ({children}) => {
+const AuthProvider: React.FC = ({ children }) => {
+   const [auth, setAuth] = useState<IAuthState>({} as IAuthState);
 
-   const[auth, setAuth] = useState<IAuthState>({} as IAuthState);
+   const signIn = useCallback(async ({ email, password }) => {
+       const response = await apiUser.login({
+           email,
+           password,
+       });
+       const { access_token, user } = response.data.data;
+       api.defaults.headers.common.Authorization = `Bearer ${access_token}`;
+       setAuth({ access_token, user });
 
-   const signIn = useCallback(async({email, password}) => {
-      const response = await apiUser.login({
-         email,
-         password,
-      });
-      const {acess_token, user} = response.data.data;
-      api.defaults.headers.common.Authorization = `Bearer ${acess_token}`;
-      setAuth({acess_token, user});
-
-      await AsyncStorage.setItem("acess_token", acess_token);
-      await AsyncStorage.setItem("user", JSON.stringify(user));
+       await AsyncStorage.setItem("access_token", access_token);
+       await AsyncStorage.setItem("user", JSON.stringify(user));
    }, []);
 
-   const register = useCallback(async({name, email, password}) => {
-      const response = await apiUser.register({
-         name,
-         email,
-         password,
-      });
-      const { access_token, user }= response.data.data;
-      api.defaults.headers.common.Authorization = `Bearer ${access_token}`;
-      setAuth({access_token, user});
+   const register = useCallback(async ({ name, email, password }) => {
+       const response = await apiUser.register({
+           name,
+           email,
+           password,
+       });
+       const { access_token, user } = response.data.data;
+       api.defaults.headers.common.Authorization = `Bearer ${access_token}`;
+       setAuth({ access_token, user });
 
-      await AsyncStorage.setItem("acess_token", access_token);
-      await AsyncStorage.setItem("user", JSON.stringify(user));
+       await AsyncStorage.setItem("access_token", access_token);
+       await AsyncStorage.setItem("user", JSON.stringify(user));
    }, []);
 
    const removeLocalStorage = async () => {
-      await AsyncStorage.removeItem("acess_token");
-      await AsyncStorage.removeItem("user");
+       await AsyncStorage.removeItem("access_token");
+       await AsyncStorage.removeItem("user");
    };
 
    const signOut = useCallback(async () => {
-      setAuth ({} as IAuthState);
-      removeLocalStorage();
-      delete api.defaults.headers.commom.authorization;
-      await apiUser.logout();
+       setAuth({} as IAuthState);
+       removeLocalStorage();
+       delete api.defaults.headers.common.authorization;
+       await apiUser.logout();
    }, []);
 
+
    const loadUserStorageData = useCallback(async () => {
-      const acess_token = await AsyncStorage.getItem("acess_token");
-      const user = await AsyncStorage.getItem("user");
-   
-   
-    if (acess_token && user) {
-       api.defaults.headers.commom.Authorization = `Bearer ${acess_token}` ;
-       setAuth({ acess_token, user: JSON.parse(user)});   
-      }
+       const access_token = await AsyncStorage.getItem("access_token");
+       const user = await AsyncStorage.getItem("user");
+
+       if (access_token && user) {
+           api.defaults.headers.common.Authorization = `Bearer ${access_token}`;
+           setAuth({ access_token, user: JSON.parse(user) });
+       }
    }, []);
 
    useEffect(() => {
-      loadUserStorageData();
+       loadUserStorageData();
    }, []);
 
    return (
-      <AuthContext.Provider
-      value={{
-         signIn,
-         signOut,
-         register,
-         acess_token: auth?.acess_token,
-         user: auth?.user,
-      }}
-      >
-        {children} 
-      </AuthContext.Provider>
+       <AuthContext.Provider
+           value={{
+               signIn,
+               signOut,
+               register,
+               access_token: auth?.access_token,
+               user: auth?.user,
+           }}
+       >
+           {children}
+       </AuthContext.Provider>
    );
-
 };
+
 function useAuth(): IAuthContextData {
    const context = useContext(AuthContext);
 
-   if(!context) {
-      throw new Error ("useAuth must be used within an AuthProvider");
+   if (!context) {
+       throw new Error("useAuth must be used within an AuthProvider");
    }
+
    return context;
 }
-export {AuthProvider, useAuth};
+export { AuthProvider, useAuth };
