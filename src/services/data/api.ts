3b59c924764f50fd43f288159ff48fa3ae4fa6dc
Giovanni <giovannisouza216@gmail.com>
2022-06-21 11:18:06 -0300

    fe

--- src/services/data/api.ts
+++ src/services/data/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
  const api = axios.create({
      baseURL: "https://laravelweb2022.herokuapp.com/api",
      headers: {
-         accept: 'appLication/json',
+         Accept: 'appLication/json',
      }
  })
 
